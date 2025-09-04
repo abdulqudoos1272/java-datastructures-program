747b24e58ad9f55bab7498e7efd15fc3f579ceb9
abdulqudoos1272 <abdulqudoos1272@gmail.com>
2025-09-04 11:49:56 +0700

    Perform Merge Sort

--- src/Program22.java
+++ src/Program22.java
@@ -36,6 +36,43 @@ class Sorting_Technique{
             arr[j+1]=key;
         }
     }
+    public void Merge_Sort(int []arr,int low,int high){
+        if(low<high){
+            int mid=((low+high)/2);
+            Merge_Sort(arr, low, mid);
+            Merge_Sort(arr, mid+1, high);
+            Merge(arr,low,mid,high);
+        }
+    }
+    private void Merge(int []arr,int low,int mid,int high){
+        int []array=new int[high-low+1];
+        int i=low,j=mid+1,k=0;
+        while(i<=mid && j<=high){
+        if(arr[i]<arr[j]){
+            array[k]=arr[i];
+            i++;
+            k++;
+        }
+        else{
+            array[k]=arr[j];
+            j++;
+            k++;
+        }
+    }
+    while(i<=mid){
+        array[k]=arr[i];
+        i++;
+        k++;
+    }
+    while(j<=high){
+        array[k]=arr[j];
+        j++;
+        k++;
+    }
+    for(int s=0; s<array.length; s++){
+        arr[low + s] = array[s];
+    }
+}
     public void display(int []arr){
         for(int i=0;i<arr.length;i++){
             System.out.print(arr[i]+" ");
@@ -58,7 +95,8 @@ public class Program22 {
         s1.display(arr);
         //s1.Bubble_Sort(arr);
         //s1.Selection_Sort(arr);
-        s1.Insertion_Sort(arr);
+        //s1.Insertion_Sort(arr);
+        s1.Merge_Sort(arr,0,(arr.length-1));
         System.out.println("After Sorting Your Array is : ");
         s1.display(arr);
     }
